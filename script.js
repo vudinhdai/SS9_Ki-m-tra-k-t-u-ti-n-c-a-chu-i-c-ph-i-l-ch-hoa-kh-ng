0a424e929f47a6593aa7b5ea685fac6bad99d0cc
vudinhdai <vudinhdai1999hnue@gmail.com>
2019-07-12 08:54:50 +0700

    tim chu hoa

---
+++ script.js
@@ -0,0 +1,11 @@
+function isFirstLetterUpperCase(str){
+      regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+        console.log("String's first character is uppercase");
+    } else {
+        console.log("String's first character is not uppercase");
+    }
+    isFirstLetterUpperCase("Nguyen van Nam");
+    isFirstLetterUpperCase("NGUYEN VAN NAM");
+    isFirstLetterUpperCase("nguyen van Nam");
+}
